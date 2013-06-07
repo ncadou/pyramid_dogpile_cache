666e60c3bb8c6bc6275d87a6b42aa584eb311cb5
Moriyoshi Koizumi <mozo@mozo.jp>
2013-06-07 16:36:12 +0900

    Initial.

---
+++ setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0'
+
+requires = [
+    'pyramid',
+    'dogpile.cache',
+    ]
+
+tests_require = [
+    'mock',
+    ]
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='altair.auth',
+      version=version,
+      description="dogpile.cache factory for Pyramid",
+      long_description=long_description,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Pyramid",
+        ],
+      keywords='web wsgi pylons pyramid dogpile cache',
+      author='Moriyoshi Koizumi',
+      author_email='mozo@mozo.jp',
+      url='http://svn.plone.org/svn/collective/',
+      license='bsd',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=requires + tests_require,
+      extras_require={
+        'testing': tests_require,
+        },
+      install_requires=requires,
+      test_suite='pyramid_dogpile_cache',
+      entry_points="""
+      # -*- Entry points: -*-
+      [dogpile.cache]
+      file = dogpile.cache.backends.file:DBMBackend
+      memcached = dogpile.cache.backends.memcached:MemcachedBackend
+      memcached.pylibmc = dogpile.cache.backends.memcached:PylibmcMemcachedBackend
+      bmemcached = dogpile.cache.backends.memcached:BMemcachedBackend
+      memory = dogpile.cache.backends.memory:MemoryBackend
+      redis = dogpile.cache.backends.redis:RedisBackend
+      """,
+      )
